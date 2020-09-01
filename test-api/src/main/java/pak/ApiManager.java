9a7688b97c8246c8aba74369f234bb917796b2b1
Nodar Sakvarelidze <nsakvarelidze12@gmail.com>
2020-09-01 18:02:03 +0400

    added rest api generation by swagger codegen maven plugin

---
+++ test-api/src/main/java/pak/ApiManager.java
@@ -0,0 +1,28 @@
+package pak;
+
+import com.swagger.client.codegen.rest.api.MyControllerGreetingApi;
+import com.swagger.client.codegen.rest.invoker.ApiClient;
+
+public class ApiManager {
+
+    private static String apiUrl = "http://localhost:8080";
+    private static boolean initialized = false;
+    private static ApiClient apiClient = null;
+    private static MyControllerGreetingApi myControllerGreetingApi = null;
+
+    private static void init(String... apiUrlArg) {
+        String url = apiUrlArg.length == 1 ? apiUrlArg[0] : apiUrl;
+        apiClient = new ApiClient().setBasePath(url);
+        myControllerGreetingApi = new MyControllerGreetingApi(apiClient);
+        initialized = true;
+    }
+
+    public static MyControllerGreetingApi getMyControllerGreetingApi(String... apiUrlArg) {
+        if (initialized) {
+            return myControllerGreetingApi;
+        }
+        init(apiUrlArg);
+        return myControllerGreetingApi;
+    }
+
+}
