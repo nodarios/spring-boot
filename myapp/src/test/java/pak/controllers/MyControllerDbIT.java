6ad44d449f662d1d9d832f5e331858c09ccc6af0
Nodar Sakvarelidze <nsakvarelidze12@gmail.com>
2020-09-22 18:53:25 +0400

    added ParameterizedTypeReference to MyControllerDbIT.java

--- myapp/src/test/java/pak/controllers/MyControllerDbIT.java
+++ myapp/src/test/java/pak/controllers/MyControllerDbIT.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
@@ -17,6 +19,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import pak.datajpa.entity.MyEntity;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -24,7 +28,7 @@ import static org.hamcrest.Matchers.equalTo;
 //@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT/*, properties = {"spring.h2.console.enabled=true", "spring.h2.console.path=/h2"}*/)
-@Sql({"/data-plus.sql"})
+@Sql(scripts = {"/data-plus.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class MyControllerDbIT {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
@@ -49,6 +53,19 @@ public class MyControllerDbIT {
     public void testGetEntities() throws Exception {
         //logger.info("port {}", port);
         //logger.info("serverPort {}", serverPort);
+
+        //ParameterizedTypeReference<List<MyEntity>> responseType =
+        //        new ParameterizedTypeReference<List<MyEntity>>() {
+        //        };
+        //ResponseEntity<List<MyEntity>> response = template.exchange(
+        //        base.toString(),
+        //        HttpMethod.GET,
+        //        null,
+        //        responseType
+        //);
+        //assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        //assertThat(response.getBody().size(), equalTo(7));
+
         ResponseEntity<MyEntity[]> response = template.getForEntity(base.toString(), MyEntity[].class);
         assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
         assertThat(response.getBody().length, equalTo(7));
