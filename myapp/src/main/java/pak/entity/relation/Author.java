91cafb4e1b72db0c13841988395b499c42fe2831
nodarios <nodarios@gmail.com>
2020-12-13 17:29:18 +0400

    minor changes

--- myapp/src/main/java/pak/entity/relation/Author.java
+++ myapp/src/main/java/pak/entity/relation/Author.java
@@ -6,9 +6,6 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Entity
--- myapp/src/main/java/pak/entity/relation/UserProfile.java
+++ myapp/src/main/java/pak/entity/relation/UserProfile.java
@@ -1,7 +1,6 @@
 package pak.entity.relation;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
