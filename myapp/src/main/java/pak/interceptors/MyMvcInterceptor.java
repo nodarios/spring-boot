6c6123bb33272bcbba5f2f39854424897ff748a4
Nodar Sakvarelidze <nodar.sakvarelidze@prt.manomano.com>
2022-09-16 14:45:37 +0400

    added MDC to MyMvcInterceptor

--- myapp/src/main/java/pak/interceptors/MyMvcInterceptor.java
+++ myapp/src/main/java/pak/interceptors/MyMvcInterceptor.java
@@ -1,6 +1,7 @@
 package pak.interceptors;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,8 +13,11 @@ public class MyMvcInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        MDC.put("customKey", "customValue");
+
         long startTime = System.currentTimeMillis();
         request.setAttribute("startTime", startTime);
+
         return true;
     }
 
@@ -24,6 +28,13 @@ public class MyMvcInterceptor implements HandlerInterceptor {
         long executeTime = endTime - startTime;
         //modelAndView.addObject("executeTime", executeTime);
         log.info("handler: {}, executeTime: {} ms", handler, executeTime);
+
+        MDC.remove("customKey");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        MDC.remove("customKey");
     }
 
 }
