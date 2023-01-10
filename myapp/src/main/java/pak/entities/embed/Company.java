959feeb5778f5de7bd5939230139e5a51430d469
Nodar Sakvarelidze <nodar.sakvarelidze@prt.manomano.com>
2023-01-10 17:56:07 +0400

    added collection table

--- myapp/src/main/java/pak/entities/embed/Company.java
+++ myapp/src/main/java/pak/entities/embed/Company.java
@@ -6,11 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.AttributeOverride;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -33,5 +37,27 @@ public class Company {
     @AttributeOverride(name = "house", column = @Column(name = "address_house"))
     @AttributeOverride(name = "postcode", column = @Column(name = "address_postcode"))
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "emails"//,
+            //joinColumns = @JoinColumn(name = "company_id", referencedColumnName = "id"),
+            //foreignKey = @ForeignKey(name = "fk_name")
+    )
+    @Column(name = "email")
+    private List<String> emails = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(
+            name = "Addresses"//,
+            //joinColumns = @JoinColumn(name = "company_id", referencedColumnName = "id"),
+            //foreignKey = @ForeignKey(name = "fk_name")
+    )
+    @AttributeOverride(name = "country", column = @Column(name = "address_country"))
+    @AttributeOverride(name = "city", column = @Column(name = "address_city"))
+    @AttributeOverride(name = "street", column = @Column(name = "address_street"))
+    @AttributeOverride(name = "house", column = @Column(name = "address_house"))
+    @AttributeOverride(name = "postcode", column = @Column(name = "address_postcode"))
+    private List<Address> Addresses = new ArrayList<>();
 
 }
